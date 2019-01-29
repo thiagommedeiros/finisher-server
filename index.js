47147f0c33223e74ef631d2afe73350cd8dd32b5
Thiago M. Medeiros <thiagomunhozmedeiros@gmail.com>
2019-01-29 13:28:36 -0200

    server: add new socket events

--- index.js
+++ index.js
@@ -5,48 +5,92 @@ const io = require('socket.io')(http)
 const users = []
 const defaultStatus = 'ready'
 
+
 //on connect one user
 const onConnect = socket => {
+
+  // add user
+  const addUser = ({ id, name }) => {
+    const userAlreadyExists = users.find(user => user.id === id) 
+    
+    if (!userAlreadyExists) {
+      if (name === 'Professor Thiago' || name === 'Professor Thomaz') {
+        socket.emit('isAdmin', true)
+
+        console.log(`Admin ${name} is connected.`)
+
+        return
+      }
+
+      users.push({
+        id, 
+        name, 
+        status: defaultStatus
+      })
+
+      console.log(`User ${name} is connected.`)
+      
+      emitUsersState()
+    }
+  }
+
+
+  // update user
+  const updateUser = data => {
+    const { id, status } = data
+
+    users.forEach(user => {
+      if (user.id === id) {
+        user.status = status
+      }
+    })
+
+    emitUsersState()
+  }
+
+
+  // emit users state
+  const emitUsersState = () => {
+    console.log('Actual state:', users)
+
+    socket.broadcast.emit('usersState', users)
+    
+    socket.emit('usersState', users)
+  }
+
+
+  // clear the users status
+  const clearUsersStatus = () => {
+    users.forEach(user => user.status = defaultStatus)
+    
+    socket.broadcast.emit('usersReady', true)
+    
+    emitUsersState()
+  }
+
+  
+  // remove user
+  const removeUser = data => {
+    const { id } = data
+
+    users.forEach((user, index) => {
+      if (user.id === id) {
+        users.splice(index, 1)
+
+        console.log(`User ${user.name} is disconnected.`)
+        
+        emitUsersState()
+      }
+    })
+  }
+
   socket.on('enter', addUser)
+  socket.on('exit', removeUser)
   socket.on('updateUser', updateUser)
   socket.on('clearUsersStatus', clearUsersStatus)
 }
 
 io.on('connection', onConnect)
-
-
-// add user
-const addUser = ({ id, name }) => {
-  const userAlreadyExists = users.find(user => user.id === id) 
-  
-  console.log('connected users:', users)
-  
-  if (!userAlreadyExists) {
-    users.push({ 
-      id, 
-      name, 
-      status: defaultStatus
-    })
-    console.log(`${name} connected.`)
-  }
-}
-
-
-// update user
-const updateUser = data => {
-  const { name, status } = data
-
-  users.forEach(user => {
-    if (user.name === name) {
-      user.status = status
-    }
-  })
-}
-
-
-// clear the users status
-const clearUsersStatus = () =>
-  users.forEach(user => user.status = defaultStatus)
 
 
 
